265eb70b75cf8a8333aced89cf25a226b6d2ef4c
TDL2627 <the.dark.lord.2627@gmail.com>
2021-06-28 15:25:24 +0200

    upload

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 2, name: 'iron man' },
+  { id: 3, name: 'thor' },
+  { id: 4, name: 'dead pool' },
+  { id: 5, name: 'wolverine' },
+  { id: 6, name: 'jean grey' },
+  { id: 7, name: 'proffesor X' },
+  { id: 8, name: 'ash ketchum' },
+  { id: 9, name: 'black panther' },
+  { id: 10, name: 'fantastic four' }
+];
